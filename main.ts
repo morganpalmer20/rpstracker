a2f34ed159eeeeda067a6145f69eef5f4d4ba0dd
morganpalmer20 <122543644+morganpalmer20@users.noreply.github.com>
2023-01-30 09:49:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onGesture(Gesture.Shake, function () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Want To Play A Game?")
+    P1 = 0
+    P2 = 0
+    Ties = 0
+    Rounds = 0
+    basic.pause(2000)
+})
+function update_scores () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player 1;" + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player 2;" + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties;" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds;" + Rounds)
+}
+let Rounds = 0
+let Ties = 0
+let P2 = 0
+let P1 = 0
+OLED.init(128, 64)
+OLED.writeStringNewLine("Want To Play A Game?")
+P1 = 0
+P2 = 0
+Ties = 0
+Rounds = 0
+basic.pause(2000)
+update_scores()
