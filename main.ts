d59dbb2aefacda73aca2958988faddd6e4abe6b0
morganpalmer20 <122543644+morganpalmer20@users.noreply.github.com>
2023-01-31 13:09:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,47 @@
+input.onButtonPressed(Button.A, function () {
+    OLED.init(128, 64)
+    P1 += 1
+    Rounds += 1
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    update_scores()
+})
+input.onButtonPressed(Button.AB, function () {
+    OLED.init(128, 64)
+    Ties += 1
+    Rounds += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    update_scores()
+})
+input.onButtonPressed(Button.B, function () {
+    OLED.init(128, 64)
+    P2 += 1
+    Rounds += 1
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+    update_scores()
+})
 input.onGesture(Gesture.Shake, function () {
+    reset()
+    basic.clearScreen()
+})
+function reset () {
     OLED.init(128, 64)
     OLED.writeStringNewLine("Want To Play A Game?")
     P1 = 0
@@ -6,26 +49,20 @@ input.onGesture(Gesture.Shake, function () {
     Ties = 0
     Rounds = 0
     basic.pause(2000)
-})
+    update_scores()
+}
 function update_scores () {
     OLED.clear()
-    OLED.writeStringNewLine("Player 1; " + P1)
+    OLED.writeStringNewLine("Player 1 ; " + P1)
     OLED.newLine()
-    OLED.writeStringNewLine("Player 2; " + P2)
+    OLED.writeStringNewLine("Player 2 ; " + P2)
     OLED.newLine()
-    OLED.writeStringNewLine("Ties; " + Ties)
+    OLED.writeStringNewLine("Ties ; " + Ties)
     OLED.newLine()
-    OLED.writeStringNewLine("Rounds; " + Rounds)
+    OLED.writeStringNewLine("Rounds ; " + Rounds)
 }
-let Rounds = 0
-let Ties = 0
 let P2 = 0
+let Ties = 0
+let Rounds = 0
 let P1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Want To Play A Game?")
-P1 = 0
-P2 = 0
-Ties = 0
-Rounds = 0
-basic.pause(2000)
-update_scores()
+reset()
